10bbe3e4a983b9c636a1a85f3b0350e78ac2bff2
Julian <julian.gierl@gmx.de>
2022-03-21 16:46:22 +0100

    Initial Commit

---
+++ SwiftUICustomViews/SwiftUICustomViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomViewsApp.swift
+//  SwiftUICustomViews
+//
+//  Created by Julian Gierl on 21.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
